13def639775b205f03203743c392a8cd3c32e257
Jan Galinski <jan.galinski@holisticon.de>
2019-07-21 22:49:37 +0200

    next dev version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 allprojects {
   group = "io.toolisticon.addons.jgiven"
-  version = "0.5.1"
+  version = "0.5.2-SNAPSHOT"
 
   apply {
     from("${rootProject.rootDir}/gradle/repositories.gradle.kts")
